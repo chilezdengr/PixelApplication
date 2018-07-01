0ddd5701cbe2ff11eb84722d80a2e319342a516b
chilezdengr <34574584+chilezdengr@users.noreply.github.com>
2018-07-01 11:34:26 +0100

    Update designs.js

--- designs.js
+++ designs.js
@@ -3,8 +3,40 @@
 
 // When size is submitted by the user, call makeGrid()
 
-function makeGrid() {
-
+function makeGrid(rows, columns) {
 // Your code goes here!
-
+  for(let i=1; i<=rows; i++){
+//selects the table and appends the rows depending on the user input
+    var tab = $('table');
+    tab.append('<tr></tr>');
 }
+//selects the tag table row<tr> to append the column
+    for(let i=1; i<=columns; i++){
+        $('tr').append('<td></td>');
+    }
+}
+// Select size input
+// .When the height and width size is submitted by the user,the makeGrid() is called
+//use the .submit method
+
+  $('form').submit(function(event){
+    let rows = $('input#inputHeight').val();
+    let columns = $('input#inputWeight').val();
+
+    // clears the DOM to append the next <tr><td>
+    $('tr').remove();
+    //calls the makeGrid() function
+    makeGrid(rows, columns);
+    event.preventDefault();
+});
+
+$('#colorPicker').change(function(event){
+    let gridColor = $(this).val();
+    $('td').click(function(event){
+        $(this).css("background-color", gridColor);
+    });
+
+    event.preventDefault();
+});
+
+
